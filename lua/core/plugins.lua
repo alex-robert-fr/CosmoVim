9f1f4a702e03342128ff873f9b661b9bcf18d142
Alex Robert <contact.alexrobert04@gmail.com>
2023-12-19 00:36:24 +0100

    ADD: Tokyonight

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,6 +13,12 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+	use {
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	}
 
   if packer_bootstrap then
     require('packer').sync()
